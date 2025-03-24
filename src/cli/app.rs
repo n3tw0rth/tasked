4b41fb373c4f328d1fb9631fa493eeb1582740b5
n3tw0rth <rtheekshanafernando@gmail.com>
2025-03-24 21:57:52 +0530

    wip: cli inline tasks list

--- src/cli/app.rs
+++ src/cli/app.rs
@@ -5,7 +5,7 @@ use crate::common::tasks::{GoogleTasks, Tasks, TasksLists};
 
 use anyhow::Result;
 use ratatui::style::{Color, Style};
-use ratatui::text::{Line, Span};
+use ratatui::text::{Line, Span, Text};
 use ratatui::widgets::Paragraph;
 
 pub struct Cli {
@@ -32,7 +32,13 @@ impl Cli {
                     let tasks = self.tasks.get_tasks_lists().await?;
                     self.cli_get_task_lists(tasks).await?
                 }
-                ListOption::Tasks => self.cli_get_tasks(self.tasks.get_tasks(&"").await?).await?,
+                ListOption::Tasks => {
+                    let tasks_list = self
+                        .tasks
+                        .get_tasks("MDYyMzk3MDkxNjkyNDIyNzU5MDE6MDow")
+                        .await?;
+                    self.cli_get_tasks(tasks_list).await?;
+                }
             },
             Some(Command::Add { value }) => {
                 println!("Adding {}", value);
@@ -55,9 +61,10 @@ impl Cli {
                 .items
                 .unwrap()
                 .iter()
-                .map(|item| {
+                .enumerate()
+                .map(|(index, item)| {
                     Line::from(vec![Span::styled(
-                        item.title.clone(),
+                        format!("{}. {}", index + 1, item.title.clone()),
                         Style::default().fg(Color::Green),
                     )])
                 })
@@ -69,7 +76,34 @@ impl Cli {
         Ok(())
     }
 
-    async fn cli_get_tasks(&self, _tasks: Tasks) -> Result<()> {
+    async fn cli_get_tasks(&self, task_lists: TasksLists) -> Result<()> {
+        Inline::new().show(|frame| {
+            let area = frame.area();
+            let lines: Vec<Line<'_>> = task_lists
+                .items
+                .unwrap()
+                .iter()
+                .enumerate()
+                .map(|(index, item)| {
+                    let list_title_line = Line::from(vec![Span::styled(
+                        format!("{}. {}", index + 1, item.title.clone()),
+                        Style::default().fg(Color::Green),
+                    )]);
+
+                    //let task_lines = item.tasks.as_ref().unwrap().iter().map(|t| {
+                    //    Line::from(vec![Span::styled(
+                    //        format!("- {}", t.title),
+                    //        Style::default().fg(Color::Green),
+                    //    )])
+                    //});
+
+                    Line::from(list_title_line)
+                })
+                .collect();
+
+            let paragraph = Paragraph::new(lines);
+            frame.render_widget(paragraph, area);
+        })?;
         Ok(())
     }
 }
--- src/common/tasks.rs
+++ src/common/tasks.rs
@@ -9,30 +9,34 @@ use crate::auth::google::GoogleOAuth;
 const TASKS_API_ENDPOINT: &str = "https://tasks.googleapis.com/tasks/v1";
 
 #[derive(Clone, Debug, Default, Deserialize)]
+#[allow(dead_code)]
 pub struct TasksLists {
     kind: String,
     etag: String,
     pub items: Option<Vec<TasksList>>,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Default)]
+#[allow(dead_code)]
 pub struct TasksList {
     kind: String,
     id: String,
     etag: String,
     pub title: String,
     updated: DateTime<Local>,
-    tasks: Option<Vec<Tasks>>,
+    pub tasks: Option<Vec<Tasks>>,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+#[allow(dead_code)]
 pub struct Tasks {
     kind: String,
     etag: String,
-    title: String,
+    pub title: String,
     notes: Option<String>,
     updated: String,
-    posistion: i8,
+    position: String,
     status: String,
     web_view_link: String,
 }
@@ -40,7 +44,12 @@ pub struct Tasks {
 /// Tasks Lists and Tasks are from two different apis, using this struct to define the additional
 /// format for tasks response and update the original TasksList with the items from this struct
 #[derive(Debug, Deserialize)]
-pub struct ArbitaryTaskList {}
+#[allow(dead_code)]
+pub struct TasksResponse {
+    kind: String,
+    etag: String,
+    pub items: Option<Vec<Tasks>>,
+}
 
 pub struct GoogleTasks {
     pub auth: Box<GoogleOAuth>,
@@ -55,8 +64,58 @@ impl GoogleTasks {
         }
     }
 
-    pub async fn get_tasks(&self, _list: &str) -> Result<Tasks> {
-        unimplemented!()
+    pub async fn get_tasks(&mut self, list_id: &str) -> Result<TasksLists> {
+        // populate the tasks lists if the data is not already fetched
+        if self.task_lists.items.iter().len() <= 0 {
+            self.get_tasks_lists().await?;
+        }
+
+        let tasks = match reqwest::Client::new()
+            .get(format!(
+                "{}{}",
+                TASKS_API_ENDPOINT,
+                format!("/lists/{}/tasks", list_id)
+            ))
+            .header(
+                "Authorization",
+                format!("Bearer {}", self.auth.get_tokens().await.unwrap()),
+            )
+            .send()
+            .await
+        {
+            Err(e) => {
+                error!("{}", e);
+                Some(vec![Tasks::default()])
+            }
+            Ok(resp) => match resp.status() {
+                StatusCode::OK => Some(resp.json::<TasksResponse>().await.unwrap().items.unwrap()),
+                StatusCode::UNAUTHORIZED => {
+                    error!("Unauthenicated");
+                    self.auth.refresh_token().await?;
+                    let _ = Box::pin(async {
+                        self.get_tasks_lists().await.ok();
+                    })
+                    .await;
+                    Some(vec![Tasks::default()])
+                }
+                _ => {
+                    error!("{:?}", resp.json::<TasksLists>().await.ok());
+                    Some(vec![Tasks::default()])
+                }
+            },
+        };
+
+        self.update_tasks(list_id, tasks.unwrap()).await?;
+        Ok(self.task_lists.clone())
+    }
+
+    async fn update_tasks(&mut self, list_id: &str, tasks: Vec<Tasks>) -> Result<()> {
+        if let Some(items) = self.task_lists.items.as_mut() {
+            if let Some(task_list) = items.iter_mut().into_iter().find(|t| t.id == list_id) {
+                task_list.tasks = Some(tasks)
+            }
+        }
+        Ok(())
     }
 
     pub async fn get_tasks_lists(&mut self) -> Result<TasksLists> {
